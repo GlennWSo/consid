1f9cdae44d1411b0136b9e87e17785e50d7bef6d
GlennWSo <sondellglenn@gmail.com>
2024-05-08 10:45:55 +0200

    clean up

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,33 @@
-use rand::prelude::*;
+use std::thread::sleep;
+use std::time::Duration;
 
-#[allow(unused)]
+use rand::distributions::{DistIter, Uniform};
+use rand::{distributions::Standard, prelude::*};
+
 use rand_chacha::ChaCha8Rng;
 
 use consid::{v4::Scanner4 as NewScanner, OldScanner, Scanner};
 
 fn main() {
-    // let mut rng = ChaCha8Rng::seed_from_u64(11);
-    let mut rng = thread_rng();
-    let temps: Vec<i32> = (0..10_u32.pow(7)).map(|_| rng.gen_range(-10..10)).collect();
-    let source = temps.clone().into_iter();
+    // data spec
+    let seed: u64 = 11;
+    let low = -20;
+    let high = 30;
+    let count = 10_usize.pow(6);
+    let rng = ChaCha8Rng::seed_from_u64(seed);
+
+    let distro = Uniform::new_inclusive(low, high);
+    let rng_gen = distro.sample_iter(rng).take(count);
+
+    // task
     let tolerance = 5;
-    let mut new_scanner = NewScanner::new(Box::new(source.clone()), tolerance);
-    let time = new_scanner.time_me();
-    let (start, end) = new_scanner.best_range();
-    if start > 0 {
-        println!("pre temp: {}", temps[start - 1]);
-    }
-    println!("new best temps: {:?}", &temps[start..end]);
-    if end < (temps.len() - 1) {
-        println!("post temp: {}", temps[end]);
-    }
-    println!("{}", new_scanner);
-    println!("new time: {:.3?}", time);
-    {
-        let mut old_scanner = OldScanner::new(Box::new(source), tolerance);
-        let time = old_scanner.time_me();
-        println!("old time: {:.3?}", time);
-        let old_range = old_scanner.best_range();
-        println!("old");
-        let (ostart, oend) = old_range;
-        if ostart > 0 {
-            println!("pre temp: {}", temps[ostart - 1]);
-        }
-        println!("best temps: {:?}", &temps[ostart..oend]);
-        if oend < (temps.len() - 1) {
-            println!("post temp: {}", temps[oend]);
-        }
-        assert_eq!((start, end), old_range, "\nleft: new");
-    }
+
+    // solve
+    let mut scanner = NewScanner::new(Box::new(rng_gen), tolerance);
+    let time = scanner.time_me();
+
+    // // report
+    let (start, end) = scanner.best_range();
+    println!("best window found {start}..={end}");
+    println! {"time taken: {}ms", time.as_millis()};
 }
